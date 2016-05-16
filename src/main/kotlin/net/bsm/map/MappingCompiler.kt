65ba763a7b2fd9d837aed94baca9f20fa0df064c
PizzaCrust <torchprojectserver@gmail.com>
2016-05-16 14:37:57 +0700

    decompiler

--- src/main/kotlin/net/bsm/map/MappingCompiler.kt
+++ src/main/kotlin/net/bsm/map/MappingCompiler.kt
@@ -1,15 +1,21 @@
 package net.bsm.map
 
-import java.io.File
-import java.io.FileOutputStream
-import java.io.ObjectOutputStream
+import java.io.*
 
-class MappingCompiler(val outputBinaryFile: File, val mappingObj: BinaryMapping) {
+class MappingCompiler(val outputBinaryFile: File, val mappingObj: BinaryMapping?) {
     fun compile() {
         val fileStream: FileOutputStream = FileOutputStream(outputBinaryFile)
         val objectStream: ObjectOutputStream = ObjectOutputStream(fileStream)
         objectStream.writeObject(mappingObj)
         objectStream.close()
         fileStream.close()
+    }
+    fun decompile() : BinaryMapping {
+        val inputStream: FileInputStream = FileInputStream(outputBinaryFile)
+        val objectStream: ObjectInputStream = ObjectInputStream(inputStream)
+        val binaryMapping: BinaryMapping = objectStream.readObject() as BinaryMapping
+        objectStream.close()
+        inputStream.close()
+        return binaryMapping
     }
 }
